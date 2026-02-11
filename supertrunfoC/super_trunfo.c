c7ac6bdf6e0bae60514a3d87bc89e98fdd5bda52
Carlos Roberto <152189760+efo-adscarlos@users.noreply.github.com>
2026-02-11 23:32:54 +0000

    banco-c-supertrunfo

---
+++ supertrunfoC/super_trunfo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    char estado[]="RO";
+
+    char cod_estado[]="69";
+
+    char cidade[10]="PVH";
+
+    int populacao=300785;
+
+    float area=1000;
+
+    double PIB=2500;
+
+    int pontos_turisticos=50;
+
+    printf("cidade: %s \n - \n", cidade);
+    printf("estado: %s \n - \n", estado);
+
+    return 0;
+
+}
