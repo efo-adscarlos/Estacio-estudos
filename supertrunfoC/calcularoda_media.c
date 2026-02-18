5fd3e314c9809bc6350bfb8d4dc388b97ca1746b
Carlos Roberto <152189760+efo-adscarlos@users.noreply.github.com>
2026-02-18 14:43:16 -0400

    fazendo a calculadora de media em C

---
+++ supertrunfoC/calcularoda_media.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+int main(){
+    float nota1, nota2, nota3;
+    float media;
+
+    printf("*** programa de calculo de Média ***\n");
+
+    printf("Digite a 1ª nota\n");
+    scanf(%f, nota1);
+
+    printf("Digite a 2ª nota\n");
+    scanf(%f, nota3);
+
+    printf("Digite a 3ª nota\n");
+    scanf(%f, nota3);
+
+    media = (nota1 + nota2 + nota3)/3;
+
+printf("A média é: %f",media);
+
+return 0;
+}
