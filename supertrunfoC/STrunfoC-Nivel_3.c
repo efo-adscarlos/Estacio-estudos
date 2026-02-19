3e7822b75b60d14304259778c1b334db5c9538b1
Carlos Roberto <152189760+efo-adscarlos@users.noreply.github.com>
2026-02-19 11:24:15 -0400

    FALTA CRIAR OS OPERADORES DE COMPARAÇÕES ENTRE AS CARTAS

---
+++ supertrunfoC/STrunfoC-Nivel_3.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+
+    ////processos de calculoos ////
+    //calculo da densidade
+
+    //A função de densidade é População / Área em km2
+
+   float calc_densidade_pop (int populacao, float area){
+    if(area <= 0){
+        return 0;
+        }
+        return populacao / area;
+    }
+
+    //A função de PIB Percapta é PIB / População
+    //calculo da PIB percapta
+   float calc_pib_percapata (float pib, int populacao){
+    if(populacao <= 0){
+        return 0;
+        }
+        return pib / populacao;
+    }
+
+    //SUPER PODER DAS CARTAS
+   float calc_superpoder (int populacao, int pontos_turisticos, float area, float pib ){
+            float soma = populacao + pontos_turisticos + area + pib;
+
+            return soma;
+    }
+
+int main(){
+
+    //Cadastro de cartas
+    // CARTA 1
+    char estado1,  codigo1[4], cidade1[50];
+    int populacao1, pontos_turisticos1;
+    float area1, pib1;
+    float pib_perc1, densidade_1;
+   
+    // CARTA 2
+    char estado2, codigo2[4], cidade2[50];
+    int populacao2, pontos_turisticos2;
+    float area2, pib2;
+    float pib_perc2, densidade_2;
+   
+
+    //interação com o usuario: vai aparecendo no console as 
+    //informações sequenciadas para cadastro das cartas
+    
+    printf("Cadastro da carta 1\n");
+
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado1);
+
+    printf("Codigo (ex: A01): ");
+    scanf("%3s", codigo1);
+
+    printf("Nome da cidade: ");
+    scanf(" %49[^\n]", cidade1);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao1);
+
+    printf("Area (km2): ");
+    scanf("%f", &area1);
+
+    printf("PIB (em bilhoes): ");
+    scanf("%f", &pib1);
+
+    printf("Pontos turisticos: ");
+    scanf("%d", &pontos_turisticos1);
+
+    printf("\nCadastro da carta 2\n");
+
+    //O & é usado para impressão de numeros inteiros e decimais
+    printf("Estado (A-H): ");
+    scanf(" %c", &estado2);
+
+    //O %3s é para limitar o campo a reconhecer apenas 3 campos da string
+    printf("Codigo (ex: B02): ");
+    scanf("%3s", codigo2);
+
+    /*o %49[^\n] vai servir para leitura do preenchimento com espaços "Rio de Janeiro"
+    Sem ele o campo faria a leitura somente "Rio" e o limite é 49 caracteres*/
+    printf("Nome da cidade: ");
+    scanf(" %49[^\n]", cidade2);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao2);
+
+    printf("Area (km2): ");
+    scanf("%f", &area2);
+
+    printf("PIB (em bilhoes): ");
+    scanf("%f", &pib2);
+
+    printf("Pontos turisticos: ");
+    scanf("%d", &pontos_turisticos2);
+
+    ////cALCULOS DE PIB Percapita e Densidade Populacional
+    pib_perc1 = calc_pib_percapata(pib1,populacao1);
+    densidade_1 = calc_densidade_pop(populacao1, area1);
+    
+    pib_perc2 = calc_pib_percapata(pib2,populacao2);
+    densidade_2 = calc_densidade_pop(populacao2, area2);
+
+    //superpoder
+    //primeiro cria a variaval que vai receber os valores em float do super poder
+    float super_poder1, super_poder2;
+
+    //aqui aplica a função para somar o super poder
+    super_poder1 = calc_superpoder(populacao1, pontos_turisticos1, area1, pib1);
+    super_poder2 = calc_superpoder(populacao2, pontos_turisticos2, area2, pib2);
+
+
+    //no final do cadastro da segunda carta ele imprime as cartas cadastradas
+    printf("\n===== CARTAS CADASTRADAS =====\n");
+    printf("\n===== CARTA 1 =====\n");
+    printf("\nCarta 1: %s\n", cidade1);
+    printf("Estado: %c\n", estado1);
+    printf("Codigo: %s\n", codigo1);
+    printf("Nome da Cidade: %s\n", cidade1);
+    printf("Populacao: %d\n", populacao1);
+    printf("Area: %.2f km2\n", area1);
+    printf("PIB: %.2f bilhoes de reais\n", pib1);
+    printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos1);
+    printf("Densidade populacional: %.2f hab/km2\n", densidade_1); ////Aqui com base nos valores informados em população e área, busca a variavel que armazenou a densidade de população
+    printf("PIB percapta: %.2f \n", pib_perc1);  ////Aqui com base nos valores informados em PIB e população, busca a variavel que armazenou o PIB Percapta
+    printf("SuperPoder : %.2f BP\n", super_poder1);
+
+    printf("\n===== CARTA 2 =====\n");
+    printf("\nCarta 2: %s\n", cidade2);
+    printf("Estado: %c\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome da Cidade: %s\n", cidade2);
+    printf("Populacao: %d\n", populacao2);
+    printf("Area: %.2f km2\n", area2);
+    printf("PIB: %.2f bilhoes de reais\n", pib2);
+    printf("Numero de Pontos Turisticos: %d\n", pontos_turisticos2);
+    printf("Densidade populacional: %.2f hab/km2\n", densidade_2);
+    printf("PIB percapta: %.2f \n", pib_perc2);
+    printf("SuperPoder : %.2f BP\n", super_poder2);
+
+    //CRIAR OS OPERADORES DE COMPARAÇÕES ENTRE AS CARTAS
+
+    
+    return 0;
+}
+
